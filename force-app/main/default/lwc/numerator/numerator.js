1805795d4fb50066b32fc17f72132e34891b8cb1
Wasif Dawar <m.wasifdawar@gmail.com>
2024-12-02 09:21:46 +0500

    In parent comp we create a counter method

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class Numerator extends LightningElement {}
+export default class Numerator extends LightningElement {
+
+    counter = 0;
+    handleIncrement() {
+      this.counter++;
+    }
+    handleDecrement() {
+      this.counter--;
+    }
+}
